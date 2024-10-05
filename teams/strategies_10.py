9461536269e0207e07a561b3b42ef1200f86185c
Quentin Chu <qc2321@columbia.edu>
2024-10-04 22:28:14 -0400

    Working basic probability-based strategy

--- teams/strategies_10.py
+++ teams/strategies_10.py
@@ -1,6 +1,8 @@
 import random
 import numpy as np
 
+from CardGame import Card, Deck, Player
+
 
 def playing(player, deck):
     """
@@ -22,33 +24,58 @@ def playing(player, deck):
 
 
 def guessing(player, cards, round):
+    """
+    Update available guesses and probabilities and return the top guesses by probability
+    """
+    # Initialize available guesses and probabilities
+    available_guesses = np.ones(52, dtype=bool)
+    probabilities = np.full(52, 1/3, dtype=float)
+
     # Update available guesses and probabilities
-    update_available_guesses(player)
-    update_probabilities(player)
-    candidate_guesses = available_guesses * probabilities
+    update_available_guesses(player, available_guesses)
+    update_probabilities(player, round, available_guesses, probabilities)
 
-    # Return the top guesses by probability
-    return candidate_guesses.argsort()[::-1][:13-round]
+    # Debug print
+    # print(f'probabilities: {probabilities}')
+    
+    # Return top guesses by probability
+    candidate_guesses = probabilities.argsort()[::-1][:13-round]
+    guesses = [card for card in cards if convert_card_to_index(card) in candidate_guesses]
+    return guesses
 
 
-def update_available_guesses(player):
+def update_available_guesses(player, available_guesses):
+    """
+    Update available guesses by removing cards in hand, played and exposed cards
+    """
     for my_card in player.hand:
         card_idx = convert_card_to_index(my_card)
         available_guesses[card_idx] = False
 
-    for exposed_card in player.exposed_cards:
-        card_idx = convert_card_to_index(exposed_card)
+    for played_card in player.played_cards:
+        card_idx = convert_card_to_index(played_card)
         available_guesses[card_idx] = False
 
+    for exposed_card_lst in player.exposed_cards.values():
+        for exposed_card in exposed_card_lst:
+            card_idx = convert_card_to_index(exposed_card)
+            available_guesses[card_idx] = False
 
-def update_probabilities(player):
-    for my_card in player.hand:
-        card_idx = convert_card_to_index(my_card)
-        probabilities[card_idx] = 0
 
-    for exposed_card in player.exposed_cards:
-        card_idx = convert_card_to_index(exposed_card)
-        probabilities[card_idx] = 0
+def update_probabilities(player, round, available_guesses, probabilities):
+    """
+    Update probabilities by various strategies
+    """
+    # Set unavailable cards to zero probability
+    probabilities[~available_guesses] = 0
+
+    # Update probabilities based on previous rounds' guesses and cVals
+    for i in range(round - 1):
+        accuracy = player.cVals[i] / (12-i)
+        for card in player.guesses[i]:
+            card_idx = convert_card_to_index(card)
+            if available_guesses[card_idx]:
+                probabilities[card_idx] = accuracy
 
 
 def convert_card_to_index(card):
@@ -57,17 +84,15 @@ def convert_card_to_index(card):
     """
     suit_idx = suit_to_idx[card.suit]
     value_idx = value_to_idx[card.value]
-            
     return value_idx * 4 + suit_idx
 
 
 """
-Global variables for Player 10
+Global variables
 """
-available_guesses = np.ones(52, dtype=bool)
-probabilities = np.full(52, 1/3, dtype=float)
+unexposed_cards = np.ones(52, dtype=bool)
 
-suit_to_idx = {"Hearts": 0, "Diamonds": 1, "Clubs": 2, "Spades": 3}
+suit_to_idx = {"Clubs": 0, "Diamonds": 1, "Hearts": 2, "Spades": 3}
 value_to_idx = {
     "2": 0,
     "3": 1,
