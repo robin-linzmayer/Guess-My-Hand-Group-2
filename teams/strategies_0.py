2ecb7c8c74a3e3763dd4d8161e28bfba45cd6973
robin-linzmayer <robin.linzmayer@columbia.edu>
2024-10-07 02:27:15 -0400

    accidentally included _0

--- teams/strategies_0.py
+++ teams/strategies_0.py
@@ -1,20 +1,11 @@
 import random
 
-test_play = {}
-test_guess = {}
-
 def playing(player, deck):
     """
     Max First strategy
     """
     if not player.hand:
         return None
-
-    test_play[player.name] = "Playing"
-    test_guess[player.name] = "Playing"
-
-    print(test_play)
-    print(test_guess)
 
     value_order = deck.values
     max_index = 0
@@ -29,10 +20,5 @@ def playing(player, deck):
     return max_index
 
 def guessing(player, cards, round):
-    test_play[player.name] = "Guessing"
-    test_guess[player.name] = "Guessing"
-
-    print(test_play)
-    print(test_guess)
 
     return random.sample(cards, 13 - round)
