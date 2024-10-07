ba2a089b17d1feeb170788a3df8f95c3a844a96f
Adithi Narayan <adithin2001@gmail.com>
2024-10-07 12:11:00 -0400

    Combined Strategy

--- teams/strategies_4.py
+++ teams/strategies_4.py
@@ -1,8 +1,9 @@
 import random
 from itertools import chain
+from collections import defaultdict
 from CardGame import Player, Deck, Card
-
-random.seed(41)
+import math
+import numpy as np
 
 PARTNERS = {
     "North": "South",
@@ -31,108 +32,127 @@ VAL_TO_NUM = {
 
 NUM_TO_SUIT = {v: k for k, v in SUIT_TO_NUM.items()}
 NUM_TO_VAL = {v: k for k, v in VAL_TO_NUM.items()}
+MIN_SUIT = {"North":-1, "South":-1, "East": -1, "West":-1}
 
-
-def generate_permutation(perm_size):
+def generate_permutation(perm_size, cards, seed):
     """Generates a permutation dictionary, each card points to one permutation"""
-    numbers = list(range(1, 53))
     perms = {}
-
-    for i in range(1, 53):
-        perms[i] = random.sample(numbers, perm_size)
+    for i in cards:
+        random.seed(seed)
+        perms[i] = random.sample(cards, perm_size)
     return perms
 
-
-def initialize_probabilities():
-    """Initilize probability dictionary"""
-    return {card: 1 / 52 for card in range(1, 53)}
-
-
-def initilize_permutations():
-    """Generates a dictionary with keys 1 to 12, values are permutations of decreasing size"""
-    result = {}
-    for key in range(1, 13):
-        perm_size = 13 - key
-        perms = generate_permutation(perm_size)
-        result[key] = perms
-    return result
-
-
-PERMUTATIONS = initilize_permutations()
-
-
-def card_to_val(card: Card):
-    """Converts suit and value into a number from 1-52"""
-    return SUIT_TO_NUM[card.suit] + VAL_TO_NUM[card.value]
-
-
-def val_to_card(val: int) -> Card:
-    """Converts a number from 1-52 into a Card object"""
-    if val < 1 or val > 52:
-        raise ValueError("Value must be between 1 and 52")
-
-    for base in [39, 26, 13, 0]:  # Clubs, Diamonds, Spades, Hearts
-        if val > base:
-            suit = NUM_TO_SUIT[base]
-            break
-
-    # Determine the value
-    card_val = val - base
-    value = NUM_TO_VAL[card_val]
-
-    return Card(suit, value)
-
-
-def update_probabilities(
-    probs: dict[int, float],
-    guess: list[Card],
-    seen: list[Card],
-    correct: int,
-    hand_size: int,
-) -> dict[int, float]:
-    """Update probability dictionary"""
-    pass
+def get_unguessed_cards(player):
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"] 
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    allcards = [Card(suit, value) for suit in suits for value in values]
+    # All possible cards except exposed cards
+    unguessedCards = []
+    for card in allcards:
+        if card not in set(chain.from_iterable(player.exposed_cards.values())) and card not in set(player.played_cards):
+            unguessedCards.append(card)
+    # Add cards exposed in this round as partner might not have had that information during exposing
+    for p in player.exposed_cards:
+        if player.exposed_cards[p]:
+            unguessedCards.append(player.exposed_cards[p][-1])
+    return unguessedCards
 
 
 def playing(player: Player, deck: Deck):
-    game_round = len(player.played_cards) + 1
-    my_cards = [card_to_val(card) for card in player.hand]
+    round = len(player.played_cards) + 1
+    unguessedCards = get_unguessed_cards(player)
+    permutations = generate_permutation(13-round, unguessedCards, 7)
+
+    # Play min suit
+    freq = defaultdict(int)
+    for card in player.hand:
+            freq[card.suit]+=1
+        
+    minSuit = min(freq, key=freq.get)
+    # The only cases where this will throw us off 13-0-0-0 [Veryyy unlikely]
+    # Other cases where player will lose points > 3 [5-4-4-0, 9-4-0-0, 8-5-0-0, 7-6-0-0]
+    # Or always we just sacrifice 1,2 or 3 max points but we get to eliminate 13 cards entirely
+    
+    if round == 1:
+        minSuitCards = [card for card in player.hand if card.suit == minSuit]
+        maxCard = max(minSuitCards, key=lambda card: VAL_TO_NUM[card.value])
+        return player.hand.index(maxCard)
 
     # Find most similar permutations to players cards
     card_index = 0
     max_sim = 0
-    for i, k in enumerate(my_cards):
-        perms = PERMUTATIONS[game_round][k]
-        sim = len((set(my_cards) & set(perms)) - {k})
+    for i, k in enumerate(player.hand):
+        perms = permutations[k]
+        sim = len((set(player.hand) & set(perms)) - {k})
         if sim > max_sim:
             card_index = i
             max_sim = sim
 
-    return card_index
+    return card_index    
+
 
 
 def guessing(player: Player, cards, round):
+    print("\n Player: ", player.name)
+    unguessed_cards = get_unguessed_cards(player)
+    permutations = generate_permutation(13-round, unguessed_cards, 7)
+    remaining_cards = []
+    for card in cards:
+        if card not in set(chain.from_iterable(player.exposed_cards.values())) and card not in set(player.hand) and card not in set(player.played_cards):
+            remaining_cards.append(card)
+    print("Remaining cards for player ", player.name, " : ", remaining_cards)
+    
+    if remaining_cards == []:
+        print("0 remaining at round", round)
+        return random.sample(cards, 13-round)
+    
+    if round == 1:
+        suit = player.exposed_cards[PARTNERS[player.name]][-1].suit
+        MIN_SUIT[player.name] = suit
+        # Eliminate suit of card played
+        remaining_cards = [card for card in remaining_cards if card.suit != suit]
 
-    perm_index = card_to_val(player.exposed_cards[PARTNERS[player.name]][-1])
+        # Now we have total max of 26 cards, the most likely suit distribution is 4432
+        # Guess 4,4,4 from remaining cards - [TODO: Can I improve using my suit distribution?]
+        suit_groups = defaultdict(list)
+        for card in remaining_cards:
+            suit_groups[card.suit].append(card)
 
-    educated_guess = (
-        set(PERMUTATIONS[round][perm_index])
-        - set(card_to_val(card) for card in player.hand)
-        - set(chain.from_iterable(player.exposed_cards.values()))
-    )
+        # Select up to 4 cards. I think there will always be atleast 4 left
+        selected_cards = [card for suit, cards in suit_groups.items() for card in random.sample(cards, min(4, len(cards)))]
+        selected_cards = selected_cards[:12]
+        return selected_cards
+    
+    else:
+        remaining_cards = [card for card in remaining_cards if card.suit != MIN_SUIT[player.name]]
+        prob = {card: (1/len(remaining_cards)) for card in remaining_cards}
 
-    guess_deck = list(
-        set(i for i in range(1, 53))
-        - set(card_to_val(card) for card in player.hand)
-        - set(
-            card_to_val(card)
-            for card in chain.from_iterable(player.exposed_cards.values())
-        )
-        - educated_guess
-    )
+        for i in range(round-1):
+            guess = player.guesses[i]
+            c = player.cVals[i]
+            remove_prob = []
+            for card in prob:
+                if card in guess:
+                    if c==0:
+                        remove_prob.append(card)
+                    else:
+                        prob[card]*= (c/len(guess))
+                else:
+                    if len(guess)-c!=0:
+                        prob[card]*= (len(guess)-c)/len(guess)
+            for card in remove_prob:
+                del prob[card]
+        mostSimP = permutations[player.exposed_cards[PARTNERS[player.name]][-1]]
+        print("Most Similar Permutation", mostSimP)
 
-    guess = random.sample(guess_deck, 13 - round - len(educated_guess))
-    educated_guess = list(educated_guess) + guess
+        boost_factor = 2
+        for val in mostSimP:
+            if val in prob:
+                prob[val]*=boost_factor
 
-    card_guesses = [val_to_card(i) for i in educated_guess]
-    return card_guesses
+        total_weight = sum(prob.values())
+        normalized_weights = [val/total_weight for val in prob.values()]
+        np.random.seed(7)
+        random_indices = np.random.choice(np.arange(len(prob.keys())), size=13-round, replace=False, p=normalized_weights)
+        sampled_cards = [list(prob.keys())[i] for i in random_indices]
+        return(sampled_cards)
