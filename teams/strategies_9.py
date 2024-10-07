9b1f905a9a5e824f55ae1daf77ffc46e19db04bc
Jinha Park <jhpark410@snu.ac.kr>
2024-10-06 23:51:45 -0400

    fill_guesses_above_min

--- teams/strategies_9.py
+++ teams/strategies_9.py
@@ -1,4 +1,6 @@
 from collections import defaultdict, deque
+from CardGame import Card, Deck, Player
+import random
 
 """
 PLAYER CLASS VARIABLES
@@ -44,6 +46,22 @@ card_val = {
     'K' : 11,
     'A' : 12,
 }
+# Use to shift card value
+card_shift = {
+    '2' : '3',
+    '3' : '4',
+    '4' : '5',
+    '5' : '6',
+    '6' : '7',
+    '7' : '8',
+    '8' : '9',
+    '9' : '10',
+    '10' : 'J',
+    'J' : 'Q',
+    'Q' : 'K',
+    'K' : 'A',
+    'A' : 'A',
+}
 # Use to get suit value
 suit_val = {
     'Spades' : 0,
@@ -78,14 +96,28 @@ player_guesses = {
 
 # anti_suit_guess = deque(['Diamonds', 'Hearts,', 'Clubs', 'Spades'])
 
+def check_possible(card, player):
+    return (card not in player.hand) and (card not in [card for cards in player.exposed_cards.values() for card in cards])
 
+def shuffle(player, deck, seed):
+    exposed_cards = [card for cards in player.exposed_cards.values() for card in cards]
+    left_cards = list(set(deck.cards) - set(exposed_cards))
+    original_cards = left_cards.copy()
+    
+    # Shuffle left_cards using the provided seed for consistency
+    random.seed(seed)
+    random.shuffle(left_cards)
+    
+    # TODO
+    mapping = 0
+    
+    return mapping
 
 # Converts a card to the index in card_probability
 # def card_to_index (suit, val):
 #     return (13 * suit_val[suit]) + card_val[val]
 # Use to get the current probability of guessing a card
 # Keys: Card Object, Val : 1/52
-
 
 # Remove cards from player.hand in card_probability 
 def remove_cards_from_hand(player, card_probability):
@@ -183,33 +215,83 @@ def playing(player, deck):
 
     return play_card
 
+# def guessing(player, cards, round):
+#     """
+#     Guesses the Anti-Suit based on teammate's exposed card
+#     """
+
+#     card_probability = {card : 1/52 for card in cards}
+#     remove_cards_from_hand(player, card_probability)
+#     remove_cards_from_exposed_cards(player, card_probability)
+#     # print("Guesses: ", player_guesses)
+#     print("Player cVals: ", player.cVals)
+#     if round > 1:
+#         update_card_probability(player, card_probability)
+
+#     num_of_guesses = 13 - round
+#     teammate_suit = player.exposed_cards[teammate[player.name]][0].suit
+#     anti_suits = deque(['Diamonds', 'Hearts', 'Clubs', 'Spades'])
+#     anti_suits.rotate(abs(anti_suits.index(teammate_suit) - (len(anti_suits) - 1)))
+#     # if not player.cVals:
+#     #     anti_suit = anti_suits[0]
+#     #     teammate_max = anti_suit
+#     # elif player.cVals[-1] == 0:
+#     #     anti_suits.rotate(round - 1)
+#     #     anti_suit = anti_suits[0]
+#     #     teammate_max = anti_suit
+#     # else:
+#     #     anti_suit = anti_suits[0]
+#     #     teammate_max = anti_suit
+#     if player.cVals and player.cVals[-1] == 0:
+#         anti_suits.rotate(round - 1)
+#         anti_suit = anti_suits[0]
+#         teammate_max = anti_suit
+#     else:
+#         anti_suit = anti_suits[0]
+#         teammate_max = anti_suit
+
+#     # print(teammate_suit, anti_suit)
+    
+
+#     # # This is a testing print
+#     # val_check_arr = []
+#     # for card in potential_guesses:
+#     #     val_check_arr.append((card.value, card.suit))
+#     # print("Potential Guesses:", val_check_arr)
+    
+#     potential_guesses = [card for card in card_probability if card.suit == teammate_max] # All 13 cards of the teammate_suit
+#     potential_guesses.sort(key=lambda x:card_val[x.value])
+#     # This is a testing print
+#     val_check_arr = []
+#     for card in potential_guesses:
+#         val_check_arr.append((card.value, card.suit))
+#     print("Potential Guesses:", val_check_arr)
+    
+#     if len(potential_guesses) < num_of_guesses:
+#         num_of_missing_cards = num_of_guesses - len(potential_guesses)
+#         extra_guesses = []
+#         for card in card_probability.keys():
+#             if card not in potential_guesses:
+#                 extra_guesses.append(card)
+#         potential_guesses.extend(sorted(extra_guesses, key=lambda x : card_probability[x], reverse=True)[:num_of_missing_cards])
+
+#     if len(potential_guesses) > num_of_guesses:
+#         potential_guesses = potential_guesses[:13-round]
+
+#     player_guesses[player.name].append(potential_guesses)
+
+#     return potential_guesses
+
 def guessing(player, cards, round):
     """
     Guesses the Anti-Suit based on teammate's exposed card
     """
-
-    card_probability = {card : 1/52 for card in cards}
-    remove_cards_from_hand(player, card_probability)
-    remove_cards_from_exposed_cards(player, card_probability)
-    # print("Guesses: ", player_guesses)
-    print("Player cVals: ", player.cVals)
-    if round > 1:
-        update_card_probability(player, card_probability)
-
-    num_of_guesses = 13 - round
+    num_of_guesses = 14 - round
+    teammate_value = player.exposed_cards[teammate[player.name]][0].value
     teammate_suit = player.exposed_cards[teammate[player.name]][0].suit
     anti_suits = deque(['Diamonds', 'Hearts', 'Clubs', 'Spades'])
     anti_suits.rotate(abs(anti_suits.index(teammate_suit) - (len(anti_suits) - 1)))
-    # if not player.cVals:
-    #     anti_suit = anti_suits[0]
-    #     teammate_max = anti_suit
-    # elif player.cVals[-1] == 0:
-    #     anti_suits.rotate(round - 1)
-    #     anti_suit = anti_suits[0]
-    #     teammate_max = anti_suit
-    # else:
-    #     anti_suit = anti_suits[0]
-    #     teammate_max = anti_suit
+
     if player.cVals and player.cVals[-1] == 0:
         anti_suits.rotate(round - 1)
         anti_suit = anti_suits[0]
@@ -218,34 +300,12 @@ def guessing(player, cards, round):
         anti_suit = anti_suits[0]
         teammate_max = anti_suit
 
-    # print(teammate_suit, anti_suit)
-    
-
-    # # This is a testing print
-    # val_check_arr = []
-    # for card in potential_guesses:
-    #     val_check_arr.append((card.value, card.suit))
-    # print("Potential Guesses:", val_check_arr)
-    
-    potential_guesses = [card for card in card_probability if card.suit == teammate_max] # All 13 cards of the teammate_suit
+    potential_guesses = [card for card in cards if (card.suit == teammate_max and check_possible(card, player))] # All 13 cards of the teammate_suit
     potential_guesses.sort(key=lambda x:card_val[x.value])
-    # This is a testing print
-    val_check_arr = []
-    for card in potential_guesses:
-        val_check_arr.append((card.value, card.suit))
-    print("Potential Guesses:", val_check_arr)
-    
-    if len(potential_guesses) < num_of_guesses:
-        num_of_missing_cards = num_of_guesses - len(potential_guesses)
-        extra_guesses = []
-        for card in card_probability.keys():
-            if card not in potential_guesses:
-                extra_guesses.append(card)
-        potential_guesses.extend(sorted(extra_guesses, key=lambda x : card_probability[x], reverse=True)[:num_of_missing_cards])
 
-    if len(potential_guesses) > num_of_guesses:
-        potential_guesses = potential_guesses[:13-round]
+    while len(potential_guesses) < num_of_guesses:
+        teammate_value = card_shift[teammate_value]
+        if check_possible(Card(teammate_suit, teammate_value), player):
+            potential_guesses.append(Card(teammate_suit, str(teammate_value)))
 
-    player_guesses[player.name].append(potential_guesses)
-
-    return potential_guesses
+    return potential_guesses[:num_of_guesses]
