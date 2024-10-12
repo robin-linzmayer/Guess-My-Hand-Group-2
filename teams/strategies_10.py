dbfc6fc82be1b2f928e4755ac52386d1b84447ad
Arman Ozcan <ao2794@columbia.edu>
2024-10-12 14:18:17 -0400

    fixed a bug

--- teams/strategies_10.py
+++ teams/strategies_10.py
@@ -11,7 +11,7 @@ def randomize_card_mapping(deck_cards, seed):
     random.seed(seed)
     
     # Shuffle the deck cards using the provided seed
-    shuffled_indices = list(range(len(deck_cards)))
+    shuffled_indices = list(range(51))
     random.shuffle(shuffled_indices)
     
     # Create a mapping of each card in the deck to a random index
@@ -23,17 +23,21 @@ def playing(player, deck):
     global seeds
     # Determine the random seed to use based on the number of cards already played
     random_seed_index = len(player.played_cards)
-    print(f"Random seed index in playing is {random_seed_index}.")
+    # print(f"Random seed index in playing is {random_seed_index}.")
     random_seed = seeds[random_seed_index]
-    print(f"Random seed in playing is {random_seed}.")
+    # print(f"Random seed in playing is {random_seed}.")
 
     # Get the random mapping for the entire deck
-    card_mapping = randomize_card_mapping(deck.copyCards, random_seed)
+    card_mapping = randomize_card_mapping(copy(deck.copyCards), random_seed)
 
     # Find the card in the player's hand that has the maximum random mapping value
-    max_card = max(player.hand, key=lambda card: card_mapping[card])
-    print(f"Max card mapping for the card {player.name}: {card_mapping[max_card]}")
 
+    # Find the card in the player's hand that has the maximum random mapping value
+    try:
+        max_card = max(player.hand, key=lambda card: card_mapping[card])
+    except KeyError as e:
+        print(f"\nKeyError: {e} - This card is not found in the card_mapping dictionary")
+        
     return player.hand.index(max_card)
 
     
@@ -79,14 +83,14 @@ def update_available_guesses(player, available_guesses, cards, round):
     for i, guess in enumerate(player.guesses):
         if player.cVals[i] == 0:
             for card in guess:
-                if card in cards:
-                    cards.remove(card)
+                card_idx = convert_card_to_index(card)
+                available_guesses[card_idx] = False
 
     for i in range(1, round+1):
         random_seed_index = i - 1
-        print(f"Random seed index in guessing is {random_seed_index}.")
+        # print(f"Random seed index in guessing is {random_seed_index}.")
         random_seed = seeds[random_seed_index]
-        print(f"Random seed in guessing is {random_seed}.")
+        # print(f"Random seed in guessing is {random_seed}.")
         card_mapping = randomize_card_mapping(copy(cards), random_seed)
         partner_card = player.exposed_cards[PARTNERS[player.name]][i-1]
         removed_cards = [card for card in copy(cards) if card_mapping[card] > card_mapping[partner_card]]
@@ -107,7 +111,10 @@ def update_probabilities(player, round, available_guesses, probabilities):
     # Update probabilities based on previous rounds' guesses and cVals
     partner_name = partner[player.name]
     opponents_names = opponents[player.name]
-    
+
+    opponents_exposed = player.exposed_cards[opponents_names[0]] + player.exposed_cards[opponents_names[1]]
+    partner_exposed = player.exposed_cards[partner_name]
+
     for i in range(round - 1):
         numerator = player.cVals[i]
         denominator = OPENING_HAND_SIZE - 1 - i
@@ -122,6 +129,28 @@ def update_probabilities(player, round, available_guesses, probabilities):
             accuracy = numerator / denominator if denominator > 0 else 0
             if available_guesses[card_idx]:
                 probabilities[card_idx] = accuracy
+
+    for i in range(len(player.guesses) - 1):
+        guess_1 = player.guesses[i]
+        guess_2 = player.guesses[i+1]
+        cval_1 = player.cVals[i]
+        cval_2 = player.cVals[i+1]
+        # if all guess_1 is a subset of guess_2 (plus one other card) and c_calue is one more:
+        if set(guess_2).issubset(set(guess_1)):
+            #print("ENTERED")
+            if cval_2 == cval_1 + 1:
+                # find the card that is not in guess_1
+                for card in guess_2:
+                    if card not in guess_1:
+                        card_idx = convert_card_to_index(card)
+                        if available_guesses[card_idx]:
+                            probabilities[card_idx] = 1
+            if cval_2 == cval_1:
+                for card in guess_2:
+                    if card not in guess_1:
+                        card_idx = convert_card_to_index(card)
+                        if available_guesses[card_idx]:
+                            probabilities[card_idx] = 0
     return probabilities
 
 
